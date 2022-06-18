9f776f7347295c098e0a4775cca53a2d34f42e54
Ari Sutarman <hansyah242@gmail.com>
2022-06-18 16:57:12 +0700

    Database Struktur

---
+++ app/Models/Note.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    protected $guarded = [];
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+}
